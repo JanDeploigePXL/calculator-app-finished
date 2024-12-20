5f0b92d8552ebc48456c1d7b09c09f6792a43546
Jan Deploige <jan.deploige@student.pxl.be>
2024-12-20 19:58:54 +0100

    changed ip

--- e2e/buttontest.spec.js
+++ e2e/buttontest.spec.js
@@ -2,12 +2,12 @@
 const { test, expect } = require('@playwright/test');
 
 test("AppHasAddButton", async ({ page }) => {
-  await page.goto("http://localhost:3000");
+  await page.goto("http://192.168.1.197:3000");
   await expect(page.getByRole('button', {name: "add"})).toBeVisible();
 })
 
 test("AppCanSubtract", async ({ page }) => {
-  await page.goto("http://localhost:3000");
+  await page.goto("http://192.168.1.197:3000");
   await page.fill('input[name="number1"]', "3");
   await page.fill('input[name="number2"]', "7");
   await page.getByRole('button', {name: "subtract"}).click();
